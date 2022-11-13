e0a922257ca0937603b639c01d6550a459aff398
PankajNaralkar <115575830+PankajNaralkar@users.noreply.github.com>
2022-11-13 23:19:58 +0530

    Add files via upload

---
+++ SqareSum.py
@@ -0,0 +1,9 @@
+class point:
+    def __init__(self, x , y , z):
+        self.x=x
+        self.y=y
+        self.z=z
+    def sqSum(self):
+        return self.x**2+self.y**2+self.z**2
+p=point(1,3,5)
+print(p.sqSum())
